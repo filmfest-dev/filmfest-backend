427c95d1d579bfb1118f60fee2033f750f19698c
tonijimenez72 <156689808+tonijimenez72@users.noreply.github.com>
2025-06-18 22:12:55 +0200

    fix(codeql): refactor Dummy class

--- src/com/filmfest/Dummy.java
+++
@@ -1,7 +0,0 @@
-package com.filmfest;
-
-public class Dummy {
-    public static void main(String[] args) {
-        System.out.println("Hello FilmFest.");
-    }
-}
---
+++ src/main/java/com/filmfest/Dummy.java
@@ -0,0 +1,7 @@
+package main.java.com.filmfest;
+
+public class Dummy {
+    public static void main(String[] args) {
+        System.out.println("Hello FilmFest!");
+    }
+}
