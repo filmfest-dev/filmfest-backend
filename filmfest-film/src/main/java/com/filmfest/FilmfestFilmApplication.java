db56e6712144b1bfe6e4a44059f1fef43ce5b601
tonijimenez72 <156689808+tonijimenez72@users.noreply.github.com>
2025-06-20 00:48:24 +0200

    feat(film): add filmfest-film microservice structure (#30)

---
+++ filmfest-film/src/main/java/com/filmfest/FilmfestFilmApplication.java
@@ -0,0 +1,13 @@
+package com.filmfest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmfestFilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmfestFilmApplication.class, args);
+	}
+
+}
