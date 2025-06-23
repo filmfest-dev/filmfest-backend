41a599ee6ec561d5995ced823fd438003085d578
isma <elsantodeplata@gmail.com>
2025-06-23 17:51:33 +0200

    added the filmfest-film microservice structure

---
+++ filmfest-user/src/test/java/com/filmfest/user/FilmfestUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filmfest.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmfestUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
