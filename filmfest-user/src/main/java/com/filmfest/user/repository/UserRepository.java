0ab83829dedcc9be625ecd1c5a3567211186d546
isma <elsantodeplata@gmail.com>
2025-06-30 17:28:14 +0200

    deleted UserRepository

--- filmfest-user/src/main/java/com/filmfest/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.filmfest.user.repository;
-
-import com.filmfest.user.model.User;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import java.util.UUID;
-
-public interface UserRepository extends ReactiveMongoRepository<User, UUID> {
-}
